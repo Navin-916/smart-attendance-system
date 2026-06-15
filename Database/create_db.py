aef2d3a50038b09e74f198a5ddddbeda3620e1e4
Navin Kumar <navinkumarp916@gmail.com>
2026-06-15 23:00:12 +0530

    Completed attendance history system

--- Database/create_db.py
+++ Database/create_db.py
@@ -8,12 +8,14 @@ conn = sqlite3.connect(db_path)
 cursor = conn.cursor()
 
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS students (
+CREATE TABLE students (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     roll_no TEXT UNIQUE NOT NULL,
-    name TEXT NOT NULL
-)
+    name TEXT NOT NULL,
+    section TEXT NOT NULL
+);
 """)
+
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS attendance (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
